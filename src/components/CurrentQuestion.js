367b4ffe7cb6d709c5442f59acbafe6c42112272
johlofg <jh.lofgren@gmail.com>
2021-04-12 12:09:33 +0200

    trying to collab

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from '../reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const dispatch = useDispatch()
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
@@ -11,6 +13,7 @@ export const CurrentQuestion = () => {
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+      <button onClick={() => dispatch(quiz.actions.goToNextQuestion())}>Next Question</button>
     </div>
   )
 }
