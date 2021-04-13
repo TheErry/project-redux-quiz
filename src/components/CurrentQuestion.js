5fedf64a7af37dc12a1a82f09ae99734971459ba
johlofg <jh.lofgren@gmail.com>
2021-04-13 10:56:28 +0200

    Feedback, summary, question counter done

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -4,33 +4,60 @@ import { quiz } from '../reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
-  const checkAnswer = useSelector((state) => state.isCorrect)
+  const checkAnswer = useSelector((state) => state.quiz.answers[state.quiz.currentQuestionIndex])
+  const summary = useSelector((state) => state.quiz.quizOver)
+  const answers = useSelector((state) => state.quiz.answers)
+  const questionCount = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex].id)
   const dispatch = useDispatch()
+
+  console.log(questionCount)
 
   const handleAnswer = (item) => {
     const answerIndexOf = question.options.indexOf(item)
     dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: answerIndexOf }))
   }
-  console.log(question)
+
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
-  if (checkAnswer) {
-    console.log('yay')
+  let a
+
+  if (checkAnswer !== undefined) {
+    if (checkAnswer.isCorrect) {
+      a = '✅'
+    } else if (!checkAnswer.isCorrect) {
+      a = '✖'
+    }
   }
 
-  return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-      {question.options.map(item => (
-        <div key={item}>
-          <input type="radio" name="answer" value={item} onClick={() => handleAnswer(item)} required />
-          <label htmlFor={item}>{item}</label>
-          {console.log(item)}
-        </div>
-      ))}
-      <button type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>Next Question</button>
-    </div>
+  return (  
+    <>  
+    {summary === false && (      
+      <div>
+        <h1>Question: {question.questionText}</h1>
+        {question.options.map((item) => (
+          <div key={item}>
+            <input type="radio" name="answer" value={item} onClick={() => handleAnswer(item)} disabled={checkAnswer} required />
+            <label htmlFor={item}>{item}</label>
+          </div>
+        ))}
+        <p>{a}</p>
+        <p>Question {questionCount} out of 5</p> 
+        <button type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>Next Question</button>
+      </div>
+        )}
+        {summary === true && (
+        <div>
+            <p>Done{console.log(answers)}</p>
+            {answers.map((answer) => (
+              <div> 
+                <p>{answer.answer}</p>
+                <p>{answer.isCorrect ? "✅ " : "✖" }</p>
+              </div>
+            ))}
+        </div>    
+        )}
+    </>
   )
 }
